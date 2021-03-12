6a0fa44bfc00b4507ad137b2bca60eb957e09365
Henne Vogelsang <hvogel@opensuse.org>
2021-03-12 16:34:30 +0100

    Introduces Keyword Index/Show
    - Add a keyword index to the frontpage
    - Add #show to KeywordsController to list projects with this keyword
    - Also rename to Keyword*s*Controller.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,7 +1,13 @@
 class AboutController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show index]
 
-  def index; end
+  def index
+    @keywords = ProjectInterest.where(project: @episode.projects).map(&:keyword).uniq.first(5) if @episode
+    @popular_keywords = ProjectInterest.where(project: @episode.projects)
+                                       .group(:keyword_id).count
+                                       .sort_by { |_id, occurance| occurance }.reverse
+                                       .last(10).map { |keyword| Keyword.find(keyword[0]) } if @episode
+  end
 
   def show; end
 end
